4053630b9349dd688822975ce39a20ffd297bca4
Lillylll <luowanxuan962@gmail.com>
2025-03-04 23:28:26 +0800

    tic-tac-toe
    A c game

--- test.c
+++ test.c
@@ -1,17 +1,187 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-int main(void)
-{
-    int a, b;
-    a = 100;
-    b = 200;
-    printf("请输入一个数字：");
-    scanf("%d", &a);
+#include <ctype.h>
+#include <time.h>
 
-    printf("a=%d\n", a);
-    printf("b=%d\n", b);
-    system("pause");
+char board[3][3];
+const char PLAYER = 'X';
+const char COMPUTER = 'O';
+
+void resetBoard();
+void printBoard();
+int checkFreeSpaces();
+void playerMove();
+void computerMove();
+char checkWinner();
+void printWinner(char winner);
+
+int main()
+{
+    char winner = ' ';
+
+    resetBoard();
+
+    while(winner == ' ' && checkFreeSpaces() != 0)
+    {
+        printBoard();
+
+        playerMove();
+        winner = checkWinner();
+        if(winner != ' ' || checkFreeSpaces() == 0)
+        {
+            break;
+        }
+        
+        computerMove();
+        winner = checkWinner();
+        if(winner != ' ' || checkFreeSpaces() == 0)
+        {
+            break;
+        }
+    }
+
+    printBoard();
+    printWinner(winner);
+    
 
     return 0;
+}
+
+void resetBoard()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            board[i][j] = ' ';
+        }
+    }
+
+}
+
+void printBoard()
+{
+    printf(" %c | %c | %c ",board[0][0],board[0][1],board[0][2]);
+    printf("\n---|---|---\n");
+    printf(" %c | %c | %c ",board[1][0],board[1][1],board[1][2]);
+    printf("\n---|---|---\n");
+    printf(" %c | %c | %c ",board[2][0],board[2][1],board[2][2]);
+    printf("\n");
+}
+
+int checkFreeSpaces()
+{
+    int freeSpaces = 0;
+
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            if(board[i][j] == ' ')
+            {
+                freeSpaces++;
+            }
+        }
+    }
+    return freeSpaces;
+}
+
+void playerMove()
+{
+    int x;
+    int y;
+
+   do
+   {
+     printf("Enter row #(1-3): ");
+    scanf("%d", &x);
+    x--;
+
+    printf("Enter column #(1-3): ");
+    scanf("%d", &y);
+    y--;
+
+    if(x < 0 || x > 2 || y < 0 || y > 2)
+    {
+        printf("Invalid move! Row and column must be between 1 and 3.\n");
+    }
+    else if(board[x][y] != ' ')
+    {
+        printf("Invalid move! That spot is already taken.\n");
+    }
+    else
+    {
+        board[x][y] = PLAYER;
+        break;
+    }
+
+   } while (1);
+   
+}
+
+void computerMove()
+{
+   //create a seed based on current time
+   srand(time(0));
+   int x;
+   int y;
+
+   if(checkFreeSpaces() > 0)
+   {
+    do
+    {
+        x = rand() % 3;
+        y = rand() % 3;
+    } while (board[x][y] != ' ');
+
+    board[x][y] = COMPUTER;
+   }
+}
+
+char checkWinner()
+{
+    //check rows
+    for(int i = 0; i < 3; i++)
+    {
+        if(board[i][0] == board[i][1] && board[i][0] == board[i][2])
+        {
+            return board[i][0];
+        }
+    }
+    //check columns;
+    for(int i = 0; i < 3; i++)
+    {
+        if(board[0][i] == board[1][i] && board[0][i] == board[2][i])
+        {
+            return board[0][i];
+        }
+    }
+    //check diagonals
+    if(board[0][0] == board[1][1] && board[0][0] == board[2][2])
+        {
+            return board[0][0];
+        }
+    if(board[0][2] == board[1][1] && board[0][2] == board[2][0])
+        {
+            return board[0][2];
+        }
+    
+    return ' ';
+}
+
+void printWinner(char winner)
+{
+    if(winner == PLAYER)
+    {
+        printf("YOU WIN!");
+    }
+    else if(winner == COMPUTER)
+    {
+        printf("YOU LOSE!");
+
+    }
+    else{
+        printf("IT'S A TIE!");
+    }
+
 }
